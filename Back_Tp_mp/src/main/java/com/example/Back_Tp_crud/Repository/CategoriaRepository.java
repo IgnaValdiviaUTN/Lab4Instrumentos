4e3739ded4e7143e3ba6108a10f399cfad49032b
Igna Valdivia <ignacio.valdivia@alumnos.frm.utn.edu.ar>
2024-06-14 19:59:14 -0300

    zipeado y organizado

--- Back_Tp_mp/src/main/java/com/example/Back_Tp_crud/Repository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Back_Tp_crud.Repository;
-
-import com.example.Back_Tp_crud.Entity.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria,Long> {
-}
